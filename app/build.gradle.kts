4b983d759b59c441157b567ee90f8ab0d707a7a8
cometoflegend <irenesg83@educastur.es>
2025-03-11 22:32:28 +0100

    calculadora finalizada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.isg.suarezireneparcial2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.isg.suarezireneparcial2"
--- app/src/main/java/com/isg/suarezireneparcial2/Calculadora.kt
+++ app/src/main/java/com/isg/suarezireneparcial2/Calculadora.kt
@@ -1,6 +1,8 @@
 package com.isg.suarezireneparcial2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,101 @@ class Calculadora : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_calculadora)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Valores para almacenar los números seleccionados
+        var numActual = ""
+        var ultimoNum = ""
+        var operador = ""
+
+        // TextView de la calculadora
+        val displaycalc = findViewById<TextView>(R.id.displayCalc)
+
+        // Botones
+        val btn0 = findViewById<Button>(R.id.btn0)
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn3 = findViewById<Button>(R.id.btn3)
+        val btn4 = findViewById<Button>(R.id.btn4)
+        val btn5 = findViewById<Button>(R.id.btn5)
+        val btn6 = findViewById<Button>(R.id.btn6)
+        val btn7 = findViewById<Button>(R.id.btn7)
+        val btn8 = findViewById<Button>(R.id.btn8)
+        val btn9 = findViewById<Button>(R.id.btn9)
+        val btndel = findViewById<Button>(R.id.btnDel)
+        val btninv = findViewById<Button>(R.id.btnInv)
+        val btndiv = findViewById<Button>(R.id.btnDividir)
+        val btnmult = findViewById<Button>(R.id.btnMult)
+        val btnsum = findViewById<Button>(R.id.btnSum)
+        val btnres = findViewById<Button>(R.id.btnResta)
+        val btncalc = findViewById<Button>(R.id.btnCalc)
+
+
+        // Lista botones
+        val numBotones = listOf(btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0)
+
+        // Se pasa el texto del botón a string y se añade a numactual
+        numBotones.forEach { button ->
+            button.setOnClickListener {
+                numActual += button.text.toString()
+                displaycalc.text = numActual
+            }
         }
+
+        // Lógica operadores
+        if (numActual!= null) {
+            btndiv.setOnClickListener {
+                ultimoNum = numActual
+                numActual = ""
+                operador = "/"}
+            btnmult.setOnClickListener {
+                ultimoNum = numActual
+                numActual = ""
+                operador = "*"}
+            btnsum.setOnClickListener {
+                ultimoNum = numActual
+                numActual = ""
+                operador = "+"}
+            btnres.setOnClickListener { ultimoNum = numActual
+                numActual = ""
+                operador = "-"}
+        } else operador = ""
+
+        // Lógica cálculo
+        btncalc.setOnClickListener {
+            if (numActual.isNotEmpty() && ultimoNum.isNotEmpty() && operador != "") {
+                var result = when (operador) {
+                    "+" -> (ultimoNum.toInt() + numActual.toInt())
+                    "-" -> (ultimoNum.toInt() - numActual.toInt())
+                    "x" -> (ultimoNum.toInt() * numActual.toInt())
+                    "/" -> (ultimoNum.toInt() / numActual.toInt())
+                    else -> 0
+                }
+                numActual = result.toString()
+                displaycalc.text = numActual
+                ultimoNum = ""
+                operador = ""
+            }
+        }
+
+        // Función borrar
+        btndel.setOnClickListener {
+            numActual = ""
+            ultimoNum = ""
+            operador = ""
+            displaycalc.text = "0"
+        }
+
+        // Función invertir
+        btninv.setOnClickListener {
+            if (numActual.isNotEmpty()) {
+                numActual = numActual.reversed()
+                displaycalc.text = numActual
+            }
+        }
+
+
+
+
     }
+
 }
